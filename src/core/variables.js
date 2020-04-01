a2d552afd5f440f248c15a6280c62c3f48a8d3a3
Sasa Sladic <sasladic@s7designcreative.com>
2020-04-02 00:30:25 +0200

    App variables for develop

--- src/core/variables.js
+++ src/core/variables.js
@@ -1,14 +1,14 @@
 /**
  * Develop(local) mode
  */
-// const URL = "http://localhost:8000";
-// const API_URL = "http://localhost:8000/api/v1";
+const URL = "http://localhost:8000";
+const API_URL = "http://localhost:8000/api/v1";
 
 /**
  * Live mode, uncomment only for live
  */
-const URL = "https://admin.nsanegaming.com/";
-const API_URL = "https://admin.nsanegaming.com/api/v1";
+// const URL = "https://admin.nsanegaming.com/";
+// const API_URL = "https://admin.nsanegaming.com/api/v1";
 
 export default {
   URL,
